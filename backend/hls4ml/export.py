2969e08e56ce76bc37d6f9b77e6815f200d9825b
AlexMontgomerie <am9215@ic.ac.uk>
2021-11-17 14:08:51 +0000

    added resource strategy to HLS4ML

--- backend/hls4ml/export.py
+++ backend/hls4ml/export.py
@@ -11,6 +11,9 @@ def export(network, model_path, output_path):
     # create the configuration for the model
     config = hls4ml.utils.config_from_keras_model(model, granularity="name")
 
+    # set a resource-based strategy
+    config["Model"]["Strategy"] = "Resource"
+
     # iterate over the nodes in the network
     for node in network.nodes:
         config["LayerName"][node]["ReuseFactor"] = network.nodes[node]["hw"].get_reuse_factor()
