dad8b3b8b09d86f03b171477b4b323416562e79f
zy18 <yuzhewen0108@163.com>
2022-01-20 12:03:41 +0000

    add a few models

--- scripts/generate_networks.py
+++ scripts/generate_networks.py
@@ -22,6 +22,40 @@ tfc.save("models/tfc.keras")
 onnx.save(onnxmltools.convert_keras(tfc, target_opset=9), "models/tfc.onnx")
 
 """
+sfc model (from FINN)
+"""
+
+sfc = keras.Sequential(
+    [
+        layers.Dense(256, input_shape=[784], activation="relu", name="ip1"),
+        layers.Dense(256, activation="relu", name="ip2"),
+        layers.Dense(256, activation="relu", name="ip3"),
+        layers.Dense(10, name="ip4")
+    ], name="sfc"
+)
+
+# save the model
+sfc.save("models/sfc.keras")
+onnx.save(onnxmltools.convert_keras(sfc, target_opset=9), "models/sfc.onnx")
+
+"""
+lfc model (from FINN)
+"""
+
+lfc = keras.Sequential(
+    [
+        layers.Dense(1024, input_shape=[784], activation="relu", name="ip1"),
+        layers.Dense(1024, activation="relu", name="ip2"),
+        layers.Dense(1024, activation="relu", name="ip3"),
+        layers.Dense(10, name="ip4")
+    ], name="lfc"
+)
+
+# save the model
+lfc.save("models/lfc.keras")
+onnx.save(onnxmltools.convert_keras(lfc, target_opset=9), "models/lfc.onnx")
+
+"""
 cnv model (from FINN)
 """
 
@@ -65,6 +99,27 @@ lenet = keras.Sequential(
 # save the model
 lenet.save("models/lenet.keras")
 onnx.save(onnxmltools.convert_keras(lenet, target_opset=9, channel_first_inputs=["conv1_input"]), "models/lenet.onnx")
+
+"""
+mpcnn (from FPGAConvNet)
+"""
+
+mpcnn = keras.Sequential(
+    [
+        layers.Conv2D(20, 5, input_shape=[32, 32, 1], name="conv1"),
+        layers.MaxPooling2D(pool_size=(2,2), strides=(2,2), name="pool1"),
+        layers.Conv2D(20, 5, name="conv2"),
+        layers.MaxPooling2D(pool_size=(2,2), strides=(2,2), name="pool2"),
+        layers.Conv2D(20, 3, name="conv3"),
+        layers.Flatten(),
+        layers.Dense(300, name="ip1"),
+        layers.Dense(6, name="ip2")
+    ]
+)
+
+# save the model
+mpcnn.save("models/mpcnn.keras")
+onnx.save(onnxmltools.convert_keras(mpcnn, target_opset=9, channel_first_inputs=["conv1_input"]), "models/mpcnn.onnx")
 
 """
 simple model (from HLS4ML)
