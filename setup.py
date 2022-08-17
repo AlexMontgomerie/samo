4cbd2d9131e2c03d55d8f84bc77090e76e490c05
AlexMontgomerie <am9215@ic.ac.uk>
2022-08-17 16:15:25 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="samo",
-    version="0.2.1",
+    version="0.2.2",
     author="Alex Montgomerie, Zhewen Yu",
     author_email="am9215@ic.ac.uk",
     description="Streaming Architecture Mapping Optimiser",
